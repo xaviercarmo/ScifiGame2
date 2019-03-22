2c3a79360665c22dd52dce903d44dabdbc8ae038
Xavier Carmo <xaviercarmo@users.noreply.github.com>
2019-03-22 23:12:26 +1100

    fixed sampling issue

--- Vulkan_glfw_3D_engine/Vulkan2D/Graphics.cpp
+++ Vulkan_glfw_3D_engine/Vulkan2D/Graphics.cpp
@@ -864,7 +864,7 @@ void Graphics::createImage(uint32_t width, uint32_t height, uint32_t mipLevels, 
 	imageInfo.tiling = tiling;
 	imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
 	imageInfo.usage = usage;
-	imageInfo.samples = msaaSamples;
+	imageInfo.samples = numSamples;
 	imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
 
 	if (vkCreateImage(device, &imageInfo, nullptr, &image) != VK_SUCCESS) {
